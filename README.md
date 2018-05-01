98fc20f21a376880fdb964b5bd3c8cff715b950e
James DeLay <jdelay.jr@gmail.com>
2018-05-01 17:31:42 -0400

    feat: prepped for testing (updated package.json and babelrc files)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hackathon Project
+
+## Testing
+
+If tests need to be moved around the ```package.json``` file script needs to be edited accordingly.
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-const LiveReloadPlugin = require('webpack-livereload-plugin')
 const isDev = process.env.NODE_ENV === 'development'
-const path = require('path')
 
 module.exports = {
-  entry: './client/index.js',
+  mode: isDev ? 'development' : 'production',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './client/index.js'
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -13,20 +15,9 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
-    }
+      }
     ]
-  },
-  // When we're in development, we can use this handy live-reload plugin
-  // to refresh the page for us every time we make a change to our client-side
-  // files. It's like `nodemon` for the front end!
-  plugins: isDev ? [new LiveReloadPlugin({appendScriptTag: true})] : []
+  }
 }
